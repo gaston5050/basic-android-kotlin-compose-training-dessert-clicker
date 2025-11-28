0673a90fd2f33e5f23eccb22182a3f425965ef43
gaston5050 <80988908+gaston5050@users.noreply.github.com>
2025-11-28 15:49:53 -0300

    Segundo Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
+val lifecycle_version = "2.5.1"
 
 android {
     namespace = "com.example.dessertclicker"
@@ -46,6 +47,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -63,8 +65,10 @@ android {
     }
 }
 
-dependencies {
 
+
+dependencies {
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${lifecycle_version}")
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.material3:material3")
@@ -73,6 +77,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${lifecycle_version}")
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
